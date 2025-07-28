0ebcbba316cd16d990ff8eb75572e6ff7d79f5a7
Jitendra Kumar Gupta <jitendrakr54@outlook.com>
2025-07-28 11:58:41 +0530

    Add url to readme

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 import { updateGuest } from "../_lib/actions";
 import { SubmitButton } from "./SubmitButton";
 
@@ -37,7 +38,7 @@ function UpdateProfileForm({ guest, children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <img
+          <Image
             src={countryFlag}
             alt="Country flag"
             className="h-5 rounded-sm"
