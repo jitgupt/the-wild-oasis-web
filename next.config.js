d1db74e87ce15cd5ca09e58d45ec514fb999dbb0
Jitendra Kumar Gupta <jitendrakr54@outlook.com>
2025-07-28 10:20:56 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://rktnjbvficgdnazuqqok.supabase.co/storage/v1/object/public/cabin-images/*"
+      ),
+    ],
+  },
+  // output: "export",
+};
+
+module.exports = nextConfig;
